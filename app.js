ebcd1f36016af0069c6b318627634c124bae29c1
dmit-s <dwhatly@gmail.com>
2023-11-23 01:46:52 +0300

    upd

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const menuBtn = document.getElementById('menu-toggler');
+const menuBtn = document.getElementById("menu-toggler");
 const handleChange = (e) => {
-    const header = document.querySelector('header');
-    if(e.target.checked){
-        header.classList.add('menuIsActive');
-    } else{
-        header.classList.remove('menuIsActive');
-    }
-}
-menuBtn.addEventListener('change', handleChange);
-
+  const header = document.querySelector("header");
+  if (e.target.checked) {
+    header.classList.add("show-menu");
+  } else {
+    header.classList.remove("show-menu");
+  }
+};
+menuBtn.addEventListener("change", handleChange);
